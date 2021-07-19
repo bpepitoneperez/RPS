4ecd8317abc11ca24ad1a0b74c18129165eb95b2
Bryan Pepitone <brryann21@gmail.com>
2021-07-19 14:41:57 -0400

    Reworked JS to add event listeners to buttons, and printed results in the DOM instead of the console.

--- script.js
+++ script.js
@@ -1,100 +1,127 @@
+let finished = false;
+let playerWins = 0;
+let computerWins = 0;
+let result = 0;
+
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        cpuHand = computerPlay();
+        result = playRound(button.id, cpuHand);
+        roundEnd(result);
+    });
+});
+
+const resultsContainer = document.querySelector('#results');
+
+const playerHand = document.createElement('p');
+resultsContainer.appendChild(playerHand);
+const computerHand = document.createElement('p');
+resultsContainer.appendChild(computerHand);
+const roundResult = document.createElement('h4');
+resultsContainer.appendChild(roundResult);
+const playerWinCount = document.createElement('p');
+resultsContainer.appendChild(playerWinCount);
+const computerWinCount = document.createElement('p');
+resultsContainer.appendChild(computerWinCount);
+const finalResult = document.createElement('h2');
+resultsContainer.appendChild(finalResult);
+
+
 function computerPlay() {
     let num = Math.floor(Math.random() * 3);
     if (num == 0) {
-        console.log("Rock");
         return "Rock";
     }
     else if (num == 1) {
-        console.log("Paper");
         return "Paper";
     }
     else {
-        console.log("Scissors");
         return "Scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    computerSelection = computerSelection.toLowerCase();
+    playerHand.textContent = ("You played " + playerSelection);
+    computerHand.textContent = ("The CPU played " + computerSelection);
+    finalResult.textContent = "";
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            console.log("This round was a draw.");
+            roundResult.textContent = "This round was a draw.";
             return 0;
         }
         else if (computerSelection == "scissors") {
-            console.log("You win! Rock beats Scissors");
+            roundResult.textContent = "You win! Rock beats Scissors";
             return 1;
         }
         else {
-            console.log("You lose! Paper beats Rock");
+            roundResult.textContent = "You Lose! Paper beats Rock";
             return -1;
         }
     }
     else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            console.log("You win! Paper beats Rock");
+            roundResult.textContent = "You win! Paper beats Rock";
             return 1;
         }
         else if (computerSelection == "scissors") {
-            console.log("You Lose! Scissors beats Paper");
+            roundResult.textContent = "You Lose! Scissors beats Paper";
             return -1;
         }
         else {
-            console.log("This round was a draw.");
+            roundResult.textContent = "This round was a draw.";
             return 0;
         }
     }
     else {
         if (computerSelection == "rock") {
-            console.log("You lose! Rock beats Scissors");
+            roundResult.textContent = "You lose! Rock beats Scissors";
             return -1;
         }
         else if (computerSelection == "scissors") {
-            console.log("This round was a draw.");
+            roundResult.textContent = "This round was a draw.";
             return 0;
         }
         else {
-            console.log("You win! Scissors beats Paper");
+            roundResult.textContent = "You win! Scissors beats Paper";
             return 1;
         }
     }
 }
 
-function game() {
-    let finished = false;
-    let playerWins = 0;
-    let computerWins = 0;
-    let result = 0;
-    while (!finished) {
-        let playerSelection = prompt("Enter your choice here");
-        playerSelection = playerSelection.toLowerCase();
-        while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-            playerSelection = prompt("Invalid Choice. Please enter your choice here");
-            playerSelection = playerSelection.toLowerCase();
-        }
-        let computerSelection = computerPlay();
-        computerSelection = computerSelection.toLowerCase();
-        result = playRound(playerSelection, computerSelection);
-        if (result > 0) {
-            playerWins++;
-        }
-        else if (result < 0) {
-            computerWins++;
-        }
-        if (computerWins > 2 || playerWins > 2) {
-            finished = true;
-        }
-        console.log("Player has " + playerWins + " wins.");
-        console.log("Computer has " + computerWins + " wins.");
+function roundEnd (result) {
+    if (result > 0) {
+        playerWins++;
     }
-    if (playerWins > 2) {
-        console.log("Congratulations you won!!!");
+    else if (result < 0) {
+        computerWins++;
+    }
+    if (computerWins > 2 || playerWins > 2) {
+        finished = true;
+    }
+    if (!finished) {
+        playerWinCount.textContent = ("You currently have " + playerWins + " wins.");
+        computerWinCount.textContent = ("The CPU currently has " + computerWins + " wins.");
     }
     else {
-        console.log("You lost and the world will be destroyed.");
+        if (playerWins > 2) {
+            finalResult.textContent = "Congratulations you won!!!";
+        }
+        else {
+            finalResult.textContent = "You lost and the world will be destroyed.";
+        }
+        playerWins = 0;
+        computerWins = 0;
+        finished = false;
     }
-    
 }
 
-game();
+
+
+
 
